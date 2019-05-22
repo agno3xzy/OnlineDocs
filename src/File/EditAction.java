d668d3b1cd5fa39aab6b82855b7a1c4503ec8b7f
LizBuptSse <598628002@qq.com>
2019-05-22 23:07:50 +0800

    1111

--- src/File/EditAction.java
+++ src/File/EditAction.java
@@ -1,4 +1,44 @@
 package File;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static com.opensymphony.xwork2.Action.SUCCESS;
+
 public class EditAction {
+
+    String path;
+    String content;
+
+    public String getPath()
+    {
+        return this.path;
+    }
+
+    public void setPath(String path)
+    {
+        this.path = path;
+    }
+
+    public String getContent()
+    {
+        return this.content;
+    }
+
+    public void setContent(String content)
+    {
+        this.content = content;
+    }
+
+    public String execute()
+    {
+
+
+
+
+        return SUCCESS;
+    }
+
+
 }
